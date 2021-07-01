8939b336903022ada1579d292afc941aadbb93ac
ta <akun.rizky.baca@gmail.com>
2021-07-02 06:40:18 +0700

    pre auth view

---
+++ application/controllers/Auth.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Auth extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('templates/auth_header');
+		$this->load->view('auth/login');
+		$this->load->view('templates/auth_footer');
+	}
+
+	public function regist()
+	{
+		$this->load->view('templates/auth_header');
+		$this->load->view('auth/regist');
+		$this->load->view('templates/auth_footer');
+	}
+}
